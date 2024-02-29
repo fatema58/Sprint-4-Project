19b8eff1b1a72407c04ac1d7af0fdfbec28c31b9
fatema58 <160182950+fatema58@users.noreply.github.com>
2024-02-28 21:45:55 -0500

    Streamlit Tryout

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import streamlit as st
 import plotly as pt
-st.header("My first VSCode Project")
+st.title("Sprint-4-Project")
+st.header("Introduction")
+st.text("This project will analyze vehicles_US dataframe to analyze the number of days a vehicle is advertised")
 
-
+st.header("Hypothesis")
+st.text("")
