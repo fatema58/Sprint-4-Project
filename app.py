f14d5340a4afc93b7e70a7df6d1580950bfae15b
Wanomir <ioan.romadin@gmail.com>
2024-02-29 06:19:34 +0300

    Ivan's commit

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 import pandas as pd
 import streamlit as st
 import plotly as pt
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('vehicles_us.csv')
+
+def brand(row):
+    """
+    Takes in a vehicle's model and returns its brand
+    """
+    
+    model = row['model']
+    model_split = model.split(' ')
+    brand = model_split[0]
+    return brand
+
+df['brand'] = df.apply(brand, axis=1)
+
 st.title("Sprint-4-Project")
 st.header("Introduction")
 st.text("This project will analyze vehicles_US dataframe to analyze the number of days a vehicle is advertised")
 
 st.header("Hypothesis")
 st.text("")
+
+st.write(df)
+
+
+fig, ax = plt.subplots(figsize=(10, 4))
+
+ax.plot()
